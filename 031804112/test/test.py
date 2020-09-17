b37c14d83e7b828d419c35985b0f33cce6e7d696
lumosqqq <70754311+lumosqqq@users.noreply.github.com>
2020-09-17 20:28:46 +0800

    update test.py

--- 031804112/test/test.py
+++ 031804112/test/test.py
@@ -4,22 +4,37 @@ from collections import Counter
 import numpy as np
 
 
+class EmptyFileError(Exception):
+    def __init__(self):
+        print("要比较的文本为空文本")
+
+
 def get_stopwords():
     # 加载停用词，用于去掉不需要的无意义的一些词
     stop_words = []
-    with open('stopword.txt', 'r', encoding='utf-8') as stop:
-        words = stop.readlines()
-        for word in words:
-            stop_words.append(word.strip())
+    try:
+        with open('stopword.txt', 'r', encoding='utf-8') as stop:
+            words = stop.readlines()
+            for word in words:
+                stop_words.append(word.strip())
+    except FileNotFoundError:
+        print("找不到stopword.txt，请把它放到main所在文件夹下")
     # 停用词去重
     stop_words = set(stop_words)
+    stop.close()
     return stop_words
 
 
 def read_file(path):
-    with open(path, 'r', encoding='utf-8') as f:
-        # 读取整个txt为一个字符串
-        content = f.read()
+    try:
+        with open(path, 'r', encoding='utf-8') as f:
+            # 读取整个txt为一个字符串
+            content = f.read()
+    except FileNotFoundError:
+        print("找不到要对比的文件")
+    if (len(content) == 0):
+        raise (EmptyFileError)
+
     # 去掉换行符和空格
     content = content.replace('\n', ' ').replace(' ', '')
     f.close()
@@ -30,6 +45,7 @@ def write_file(path, content):
     with open(path, 'w', encoding='utf-8') as f:
         f.write(content)
     f.close()
+
 
 def text2vector(text, stopwords):
     # 分词，去停用词（停用词含常用标点）
@@ -48,11 +64,11 @@ def cos_similarity(v1, v2):
     else:
         return 0
 
-
 def simi(original,compare,result):
     original=original
     compare=compare
-    result=result# 读取文件
+    result=result
+    # 读取文件
     ori_text = read_file(original)
     comp_text = read_file(compare)
 
@@ -73,9 +89,9 @@ def simi(original,compare,result):
 
     # 计算相似度
     sim = cos_similarity(ori_vector, comp_vector)
-    sim1="%.2f" % sim
-    sim2=str(sim1)
-    print(original, compare, sim2)
+    sim1 = "%.2f" % sim
+    sim2 = str(sim1)
 
     # 写入文件
     write_file(result, sim2)
+    print(original,compare,sim2)
